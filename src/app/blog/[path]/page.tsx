1566a4a316e0dd840f3cc2965d3b86211da014f0
David Hoffmann <dave@dhoff.net>
2024-03-07 09:56:58 -0600

    update projects

--- src/app/blog/[path]/page.tsx
+++ src/app/blog/[path]/page.tsx
@@ -1,17 +1,6 @@
-import "../../../../styles/styles.css";
 import Nav from "../../../../components/Nav";
 
 export default function Blog() {
-
-    const baseUrl = process.env.NODE_ENV === 'production' ? 'http://dhoff.net' : 'http://localhost:3001';
-
-    async function fetchData() {
-        const response = await fetch(`${baseUrl}/api/routes`);
-        const data = await response.json();
-        console.log(data);
-    }
-
-    fetchData();
 
     return (
         <>a
